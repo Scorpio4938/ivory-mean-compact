57b88400b0f55b5abca2f8de537c903d75c7074d
mac02 <mac02@mac-pi128>
2026-07-19 09:14:09 +0000

    add CfgVehicles overrides for CarAttenuation, keep Meanscars as requiredAddons

--- Addons/mean_patch/config.cpp
+++ Addons/mean_patch/config.cpp
@@ -9,6 +9,17 @@ class CfgPatches
     };
 };
 
+// Override Mean vehicle attenuation so siren audio is audible inside the cabin
+class CfgVehicles
+{
+    class M_CVPIbase: M_CVPIbase { attenuationEffectType = "DefaultAttenuation"; };
+    class M_Charger12base: M_Charger12base { attenuationEffectType = "DefaultAttenuation"; };
+    class M_Tahoebase: M_Tahoebase { attenuationEffectType = "DefaultAttenuation"; };
+    class M_Ambulancebase: M_Ambulancebase { attenuationEffectType = "DefaultAttenuation"; };
+    class M_FPISbase: M_FPISbase { attenuationEffectType = "DefaultAttenuation"; };
+    class M_Silveradobase: M_Silveradobase { attenuationEffectType = "DefaultAttenuation"; };
+};
+
 class CfgFunctions
 {
     class mean_patch
